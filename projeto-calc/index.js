310cc0fe5d24d325f06f7868050659764afd4428
smuelmfs <smuelmfs76@gmail.com>
2023-11-16 13:10:11 -0300

    Modularizando Projeto 'projeto-calc'

--- projeto-calc/index.js
+++ projeto-calc/index.js
@@ -1,76 +1,18 @@
-const main = document.querySelector('main')
-const root = document.querySelector(':root')
-const input = document.getElementById('input')
-const resultInput = document.getElementById('result')
-
-const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+import calculate from "./js/calculate.js"
+import copyToClipBoard from "./js/copyToClipBoard.js"
+import { handleButtonClear, handleButtonPress, handleTyping } from "./js/keyHandlers.js"
+import themeSwitch from "./js/themeSwitch.js"
 
 document.querySelectorAll('.charKey').forEach((charKeyBtn) => {
-    charKeyBtn.addEventListener('click', () => {
-        const value = charKeyBtn.dataset.value
-        input.value += value
-    })
+    charKeyBtn.addEventListener('click', handleButtonPress)
 })
 
-document.getElementById('clear').addEventListener('click', () => {
-    input.value = ''
-    input.focus()
-})
+document.getElementById('clear').addEventListener('click', handleButtonClear)
 
-function calculate() {
-
-    resultInput.value = 'ERROR'
-    resultInput.classList.add('error')
-    const result = eval(input.value)
-    resultInput.value = result
-    resultInput.classList.remove('error')
-
-}
-
-input.addEventListener('keydown', (ev) => {
-    ev.preventDefault()
-
-    if (allowedKeys.includes(ev.key)) {
-        input.value += ev.key
-        return
-    }
-
-    if (ev.key === 'Backspace') {
-        input.value = input.value.slice(0, -1)
-    }
-
-    if (ev.key === 'Enter') {
-        calculate()
-    }
-})
+document.getElementById('input').addEventListener('keydown', handleTyping)
 
 document.getElementById('equal').addEventListener('click', calculate)
 
-document.getElementById('copyToClipboard').addEventListener('click', (ev) => {
-    const button = ev.currentTarget
+document.getElementById('copyToClipboard').addEventListener('click', copyToClipBoard)
 
-    if (button.innerText === 'Copy') {
-        button.innerText = 'Copied!'
-        button.classList.add('success')
-        navigator.clipboard.writeText(resultInput.value)
-    } else {
-        button.innerText = 'Copy'
-        button.classList.remove('success')
-    }
-})
-
-document.getElementById('themeSwitcher').addEventListener('click', () => {
-    if (main.dataset.theme === "dark") {
-        root.style.setProperty("--bg-color", "#f1f5f9")
-        root.style.setProperty("--border-color", "#aaa")
-        root.style.setProperty("--font-color", "#212529")
-        root.style.setProperty("--primary-color", "#26834a")
-        main.dataset.theme = "light"
-    } else {
-        root.style.setProperty("--bg-color", "#212529")
-        root.style.setProperty("--border-color", "#666")
-        root.style.setProperty("--font-color", "#f1f5f9")
-        root.style.setProperty("--primary-color", "#4dff91")
-        main.dataset.theme = "dark"
-    }
-})
+document.getElementById('themeSwitcher').addEventListener('click', themeSwitch)
